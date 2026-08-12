f086347fdc90d713e8c53263aeb0516f5ba237e5
Gustavo Augusto <gustavoags2000@gmail.com>
2026-08-11 21:27:03 -0300

    feat: lift playlist state to App and connect Add button via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import BuscaMusica from "./components/BuscaMusica";
 
 function App() {
+  const [playlist, setPlayList] = useState([]);
+
+  function addInPlaylist(music) {
+    //código para adicionar uma música na playlist
+    setPlayList([...playlist, music]);
+  }
   return (
     <div>
       <h1>Dj Rodrigão</h1>
-      <BuscaMusica />
+      <BuscaMusica playlist={playlist} onAdd={addInPlaylist} />
     </div>
   );
 }
--- src/components/BuscaMusica.jsx
+++ src/components/BuscaMusica.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function BuscaMusica() {
+function BuscaMusica({ playlist, onAdd }) {
   const [inputText, setInputText] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -32,11 +32,14 @@ function BuscaMusica() {
           placeholder="Digite o nome da música ou do artista"
         />
         <button type="submit">Buscar</button>
-        {searchResults.map((musica) => (
-          <div key={musica.trackId}>
-            <p>{musica.trackName}</p>
-            <p>{musica.artistName}</p>
-            <img src={musica.artworkUrl100} alt="Capa da música" />
+        {searchResults.map((music) => (
+          <div key={music.trackId}>
+            <p>{music.trackName}</p>
+            <p>{music.artistName}</p>
+            <img src={music.artworkUrl100} alt="Capa da música" />
+            <button type="button" onClick={() => onAdd(music)}>
+              Adicionar
+            </button>
           </div>
         ))}
       </form>
